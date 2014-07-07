2bc844901ec9a01969a9ad889db7a05a2f6e0870
Michael Leahy <mikel9286@hotmail.com>
2014-07-07 01:35:31 -0400

    hooking up the slideshow widget

--- js/Main.js
+++ js/Main.js
@@ -1,21 +1,38 @@
 ﻿(function (Main, $, undefined) {
 
-    Main.IsSafari = function () {
-        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1)
-            return true;
-        return false;
-    }
+    $('.nav a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
 
-    Main.AttachEventListeners = function () {
-        $(window).bind('resize orientationchange', function () {
-            $('footer').css('display', 'block');
+    $(function () {
+        $(".rslides").responsiveSlides({
+            manualControls: $('.slideshow-widget li'),
+            before: function () {
+                $('.slideshow-widget .rslides_here a').tab('show');
+            }
         });
+    });
 
-        $(window).bind('load', function () {
-            $('footer').css('display', 'block');
-        });
-    }
 
-    Main.AttachEventListeners();
+    //old 
+
+    //Main.IsSafari = function () {
+    //    if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1)
+    //        return true;
+    //    return false;
+    //}
+
+    //Main.AttachEventListeners = function () {
+    //    $(window).bind('resize orientationchange', function () {
+    //        $('footer').css('display', 'block');
+    //    });
+
+    //    $(window).bind('load', function () {
+    //        $('footer').css('display', 'block');
+    //    });
+    //}
+
+    //Main.AttachEventListeners();
 
 } (window.Main = window.Main || {}, jQuery))
