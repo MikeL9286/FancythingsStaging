80efe0a82cb66605cf75b8beeff144535ed00cf8
Michael Leahy <mikel9286@hotmail.com>
2014-07-11 14:08:30 -0400

    Adding template for recent posts

--- Scripts/templates/blogPostsTemplate.js
+++ Scripts/templates/blogPostsTemplate.js
@@ -1,9 +1,12 @@
 ﻿var source1 = $("#nav-post-titles-template").html();
 var source2 = $("#slideshow-images-template").html();
-//var source3 = $("#slideshow-content-template").html();
+var source3 = $("#top-posts-template").html();
+//var source3 = $("#recent-posts-template").html();
+
 var template1 = Handlebars.compile(source1);
 var template2 = Handlebars.compile(source2);
-//var template3 = Handlebars.compile(source3);
+var template3 = Handlebars.compile(source3);
+//var template4 = Handlebars.compile(source4);
 
 var data = { posts: Blogger.blog.items };
 
@@ -17,4 +20,5 @@ Handlebars.registerHelper('summary', function (postContent) {
 
 $('#nav-post-titles-template').parent().append(template1(data));
 $('#slideshow-images-template').parent().append(template2(data));
-//$('#slideshow-content-template').parent().append(template3(data));
+$('#top-posts-template').parent().append(template3(data));
+//$('#recent-posts-template').parent().append(template4(data));
