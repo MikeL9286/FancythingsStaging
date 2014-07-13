76ce0c2d2ecaa44eac2f8050826a3fe3a69b56de
Michael Leahy <mikel9286@hotmail.com>
2014-07-13 14:57:35 -0400

    hooking up a basic show more posts button

--- Scripts/Blogger.js
+++ Scripts/Blogger.js
@@ -1,20 +1,12 @@
 ﻿(function (Blogger, $, undefined) {
 
     Blogger.blog = {};
+    Blogger.nextPageToken;
 
     Blogger.GetPosts = function (ctrl) {
-        var url;
-
-        if (ctrl === undefined)
-            url = 'https://www.googleapis.com/blogger/v3/blogs/5073145937869562696/posts?maxResults=4&key=AIzaSyBxl86QJ7gRccq_egFmP3J6Zhy3cQLluIk';
-        else if (ctrl.selectedIndex !== 0)
-            url = ctrl.options[ctrl.selectedIndex].value;
-        else
-            return;
-
         $.ajax({
             type: "GET",
-            url: url,
+            url: 'https://www.googleapis.com/blogger/v3/blogs/5073145937869562696/posts?maxResults=4&key=AIzaSyBxl86QJ7gRccq_egFmP3J6Zhy3cQLluIk',
             dataType: "json",
             async: false,
             processData: "false",
@@ -26,6 +18,36 @@
             },
             success: function (data) {
                 Blogger.blog = data;
+                Blogger.nextPageToken = data.nextPageToken;
+            }
+        });
+    };
+
+    Blogger.ShowMorePosts = function () {
+        $.ajax({
+            type: "GET",
+            url: 'https://www.googleapis.com/blogger/v3/blogs/5073145937869562696/posts?maxResults=4&pageToken=' + Blogger.nextPageToken + '&key=AIzaSyBxl86QJ7gRccq_egFmP3J6Zhy3cQLluIk',
+            dataType: "json",
+            async: false,
+            processData: "false",
+            beforeSend: function (jqXHR, settings) {
+                //start timer gif
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("error: " + textStatus);
+            },
+            success: function (data) {
+                //add the new posts to the existing post list
+                //Array.prototype.push.apply(Blogger.blog.items, data.items);
+                
+                var source = $('#post-feed-template').html();
+                var template = Handlebars.compile(source);
+                var context = { posts: data.items };
+                var html = template(context);
+                
+                $('#post-feed-template').parent().append(html);
+                Blogger.nextPageToken = data.nextPageToken;
+                console.log(data);
             }
         });
     };
--- Scripts/templates/blogPostsTemplate.js
+++ Scripts/templates/blogPostsTemplate.js
@@ -1,12 +1,10 @@
 ﻿var source1 = $("#nav-post-titles-template").html();
 var source2 = $("#slideshow-images-template").html();
-var source3 = $("#top-posts-template").html();
-//var source3 = $("#recent-posts-template").html();
+var source3 = $("#post-feed-template").html();
 
 var template1 = Handlebars.compile(source1);
 var template2 = Handlebars.compile(source2);
 var template3 = Handlebars.compile(source3);
-//var template4 = Handlebars.compile(source4);
 
 var data = { posts: Blogger.blog.items };
 
@@ -14,11 +12,20 @@ Handlebars.registerHelper('firstImage', function (postContent) {
     return postContent.match('<img.* src=".*"')[0].match('http.*jpg|http.*png');
 });
 
-Handlebars.registerHelper('summary', function (postContent) {
-    return $(postContent).text().substring(0, 250);
+Handlebars.registerHelper('summary', function (postContent, length) {
+    var centerTextToRemove = postContent.match('<center>.*</center>');
+    var smallTextToRemove = postContent.match('<span.*</span>');
+    var content = postContent.split(centerTextToRemove).join(' ').split(smallTextToRemove).join(' ');
+
+    return content.length > length ?
+        $(content).text().substring(0, length) :
+        $(content).text();
+});
+
+Handlebars.registerHelper('relativeDate', function (publishedDate) {
+    return moment(publishedDate).startOf('day').fromNow();
 });
 
 $('#nav-post-titles-template').parent().append(template1(data));
 $('#slideshow-images-template').parent().append(template2(data));
-$('#top-posts-template').parent().append(template3(data));
-//$('#recent-posts-template').parent().append(template4(data));
+$('#post-feed-template').parent().append(template3(data));
