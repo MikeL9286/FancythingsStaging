f6d6c426123b9dd763c756d2e4e838ca056fea2e
Michael Leahy <mikel9286@hotmail.com>
2014-09-28 20:33:19 -0400

    fixing share icons on blogpost.html

--- Scripts/blogpost.js
+++ Scripts/blogpost.js
@@ -5,13 +5,19 @@
     //add pinterest button to images
     $('.post-content img:not([src*=images\\.rewardstyle],.post-thumbnail)').each(function (index) {
         var image = $(this);
+
+        //filter the share-icons from getting the pin button
+        if (image.parent().find('> .share-icons').length > 0) {
+            return;
+        }
+
         var imageSource = image.attr('src');
 
         var pinitLink = 'http://pinterest.com/pin/create/button/?url=' + window.location.href + '&media=' + imageSource + '&description=' + $('.post-header h3').text();
         var pinitButton = '<a href="' + pinitLink + '"><i class="fa fa-pinterest"></i></a>';
 
-        image.wrap('<div style="position:relative"></div>');
-        image.after('<div class="share-icons">' + pinitButton + '</div>');
+        image.wrap('<span style="position:relative"></span>');
+        image.after('<span class="share-icons">' + pinitButton + '</span>');
     });
 
 }(window.BlogPost = window.BlogPost || {}, jQuery))
