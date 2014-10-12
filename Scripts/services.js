03e5a825745c5b278db0a7c8fe5add96b69b97c5
Michael Leahy <mikel9286@hotmail.com>
2014-10-12 16:43:42 -0400

    Restyling services page

---
+++ Scripts/services.js
@@ -0,0 +1,32 @@
+﻿(function (Services, $, undefined) {
+
+    $(window).resize(function () {
+        resizeServices();
+    });
+
+    function resizeServices() {
+        var services = $('.service');
+        var desiredHeight = 0;
+
+        //clear the min-height if we're in a single column view
+        if (window.innerWidth < 768 && services.css('min-height') != '0px') {
+            services.css('min-height', '');
+            return;
+        }
+
+        //else set the min-height so that all rows are equal heights
+        if (window.innerWidth >= 768) {
+            services.each(function (i) {
+                var serviceHeight = $(this).outerHeight();
+                desiredHeight = (serviceHeight > desiredHeight) ? serviceHeight : desiredHeight;
+            });
+
+            services.css('min-height', desiredHeight + 5);
+        }
+    };
+
+    $(document).ready(function() {
+        resizeServices();
+    });
+
+}(window.Services = window.Services || {}, jQuery))
